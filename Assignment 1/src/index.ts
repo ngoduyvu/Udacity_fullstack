c8bc5ba4b8bf8020835d25489d647b2e697a5e7f
ngoduyvu <ngoduyvu@gmail.com>
2021-09-27 21:08:47 +1000

    Move packages around in package.json

--- Assignment 1/src/index.ts
+++ Assignment 1/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { runInContext } from 'vm';
+import sharp from 'sharp';
 
 const app = express();
 const port = 3000;
@@ -11,3 +12,8 @@ app.get('/api', (req, res) => {
 app.listen(port, () => {
   console.log(`Server started at localhost: ${port}`);
 });
+
+let resize = async function(imagePath: string, size: number, outputPath: string) {
+  return await sharp(imagePath).resize(size).toFile(outputPath);
+
+} 
