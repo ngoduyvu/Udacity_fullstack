de3ed52e8bc4c945dfc25c0edb842805dd1a3a2e
ngoduyvu <ngoduyvu@gmail.com>
2022-06-13 08:18:26 +1000

    Remove Assignment 1

--- Assignment 1/src/utilities/resize.ts
+++
@@ -1,9 +0,0 @@
-import sharp from 'sharp';
-
-const resize = async function(filename: string, width: number, height: number) {
-  await sharp('./full/' + filename + '.jpg')
-      .resize(width, height)
-      .toFile('./thumb/' + filename + '_' + height + '_'+ width + '.jpg');
-};
-
-export default resize;
