3593a794c497870ef510fb7b8e93b3712e4627f9
ngoduyvu <ngoduyvu@gmail.com>
2022-07-10 13:17:54 +1000

    Update new code

--- Assignment 3/src/app/services/product.service.ts
+++ Assignment 3/src/app/services/product.service.ts
@@ -14,29 +14,5 @@ export class ProductService {
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>("assets/data.json");
   };
-
-  getProductsTest() {
-    return [
-      {
-        id: 1,
-        name: 'Pandas',
-        price: 1000,
-        url: 'www.google.com',
-        quantity: 3,
-        description: 'This is test 1' 
-      },
-      {
-        id: 2,
-        name: 'Elephant',
-        price: 9000,
-        url: 'www.yandex.com',
-        quantity: 2,
-        description: 'This is test 2' 
-      }
-    ]
-  }
-
-  // getProductById(id: number): Observable<Product> {
-  //   return this.getProducts().pipe((map(products => products.filter(product => product.id === id)[0])));
-  // } 
+  
 }
